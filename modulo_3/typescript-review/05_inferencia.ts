08b1cc6f71fb904ef724e578021cec314d424964
ronaldo <ronaldogulotoo@gmail.com>
2026-06-17 14:56:34 -0500

    Clase 17-06-2026

---
+++ modulo_3/typescript-review/05_inferencia.ts
@@ -0,0 +1,12 @@
+let ciudad  = "Barcelona";
+const altura  = 183.5;
+const casado  = false;
+const hijos   = 2;
+const empleado = "Alejandro";
+const cargo = "Programador Sr";
+const edad = 20;
+
+console.log(`${ciudad}, ${altura}cm, casado: ${casado}, hijos: ${hijos}`);
+console.log(`${empleado}, ${cargo}, ${edad}`);
+
+ciudad = "Madrid";
